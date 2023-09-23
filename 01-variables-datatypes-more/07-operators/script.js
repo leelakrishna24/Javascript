c9fa41612bc854b3791063d45a5b0ddb8609c868
leelakrishna24 <56736849+leelakrishna24@users.noreply.github.com>
2023-09-23 23:46:17 +0530

    new file:   01-variables-datatypes-more/07-operators/index.html 	new file:   01-variables-datatypes-more/07-operators/script.js 	new file:   01-variables-datatypes-more/08-type-coersion/index.html 	new file:   01-variables-datatypes-more/08-type-coersion/script.js 	new file:   01-variables-datatypes-more/09-working-with-strings/index.html 	new file:   01-variables-datatypes-more/09-working-with-strings/script.js

---
+++ 01-variables-datatypes-more/07-operators/script.js
@@ -0,0 +1,50 @@
+// 1.Arithmetic Operators
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatination
+x = 'Hello' + ' world';
+x = 'Hello' + ' ' + 'world';
+
+// Exponent
+x = 2 ** 3
+
+// Increment
+x = 1;
+x = x + 1;
+x++;
+
+// Decrement
+x = x - 1;
+x--;
+
+// 2.Assignment Operators
+
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 4;
+x **= 5;
+
+// 3.Comparison Operators
+
+x = 2 == '2'; // does not evalutes the value //
+x = 2 === 2; // Evaluates the value //
+
+x = 2 != 2; // False //
+x = 2 != 3; // True //
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 >= 5;
+x = 10 <= 5;
+
+console.log(x);
